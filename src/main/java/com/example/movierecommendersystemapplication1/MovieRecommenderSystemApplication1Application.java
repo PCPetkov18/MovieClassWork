8e33c13aefacd8f4f6f297519f615d394ccae898
PCPetkov18 <PCPetkov18@codingburgas.bg>
2022-10-24 12:06:09 +0300

    Initial commit

---
+++ src/main/java/com/example/movierecommendersystemapplication1/MovieRecommenderSystemApplication1Application.java
@@ -0,0 +1,13 @@
+package com.example.movierecommendersystemapplication1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderSystemApplication1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieRecommenderSystemApplication1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/movierecommendersystemapplication1/MovieRecommenderSystemApplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movierecommendersystemapplication1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommenderSystemApplication1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
